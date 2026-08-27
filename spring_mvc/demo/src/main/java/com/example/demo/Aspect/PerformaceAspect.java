701c304b6f2c231976deab9b30b893d181235dfc
Gaurav7131 <thakaregaurav7131@gmail.com>
2026-08-27 15:45:27 +0530

    Implemented @EnableAspectAutoProxy successfuly

--- spring_mvc/demo/src/main/java/com/example/demo/Aspect/PerformaceAspect.java
+++ spring_mvc/demo/src/main/java/com/example/demo/Aspect/PerformaceAspect.java
@@ -1,8 +1,12 @@
 package com.example.demo.Aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +17,24 @@ public class PerformaceAspect {
     // Pointcut:predicate exp matches joinPoint.(method execution for spring aop)
     @Pointcut("execution(* com.example.demo.service.*.*(..))")
     public void serviceLayerPointcut() {
+    }
+
+    // Before:Executes before method entry
+    @Before("serviceLayerPointcut()")
+    public void logEntry(JoinPoint joinPoint) {
+        System.out.println("Before:method entering:" + joinPoint.getSignature().getName());
+    }
+
+    // AfterReturning
+    @AfterReturning(pointcut = "serviceLayerPointcut()", returning = "result")
+    public void logSuccess(JoinPoint joinPoint, Object result) {
+        System.out.println("After returning:" + joinPoint.getSignature().getName() + "returned" + result);
+    }
+
+    // AfterThrowing
+    @AfterThrowing(pointcut = "serviceLayerPointcut()", throwing = "ex")
+    public void LogError(JoinPoint joinPoint, Throwable ex) {
+        System.out.println("After throwing exception:" + joinPoint.getSignature().getName() + ":" + ex.getMessage());
     }
 
     // Around advices method exection time,target invocation,ctler jp.proceed()
